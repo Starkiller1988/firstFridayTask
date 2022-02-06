db8881668ad7d6d0267044cc070acd130cf448c7
Samir Schabel <90188949+Starkiller1988@users.noreply.github.com>
2022-02-06 21:57:27 +0100

    ,

--- src/main/java/Task/PasswordValidation.java
+++ src/main/java/Task/PasswordValidation.java
@@ -1,2 +1,28 @@
-package Task;public class PasswordValidation {
+package Task;
+
+public class PasswordValidation {
+
+    public static boolean passwordCheck(String password){
+        if(password.length() <= 5){
+            return false;
+        }
+
+    if (passNumber (password)){
+        return true;
+    } else {
+        return false;
+    }
+    }
+
+    private static boolean passNumber(String password){
+        for(int p = 0; p < password.length(); p++){
+            char letter = password.charAt(p);
+            if (letter >= 34 && letter <= 46){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 }
--- src/test/java/Task/PasswordValidationTest.java
+++ src/test/java/Task/PasswordValidationTest.java
@@ -1,4 +1,37 @@
+package Task;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PasswordValidationTest {
-  
+
+    @Test
+    void ifPasswordToSmall (){
+        boolean result = PasswordValidation.passwordCheck("abcdefghijklmnopqrstuvwxyz");
+        assertFalse(result);
+    }
+
+    @Test
+    void ifPasswordHasNumbers (){
+        boolean result = PasswordValidation.passwordCheck("1234567890");
+        assertFalse(result);
+    }
+
+    @Test
+    void ifPasswordIsValid (){
+        boolean result = PasswordValidation.passwordCheck("justic3_!m4n");
+        assertFalse(result);
+    }
+
+    @Test
+    void ifPasswordShouldHaveNumbers (){
+        boolean result = PasswordValidation.passwordCheck("SamSch1988+*");
+        assertFalse(result);
+    }
+
+
+
+
+
 }
